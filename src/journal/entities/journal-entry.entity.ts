15ab8312c4f0c951d876648505cf5d563e8b9f0c
dan-mutua <danmutua15@gmail.com>
2024-07-08 23:35:20 +0300

    add swagger documentation in  journal entinty

--- src/journal/entities/journal-entry.entity.ts
+++ src/journal/entities/journal-entry.entity.ts
@@ -1,19 +1,34 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Journal {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    description: 'The title',
+  })
   @Column()
   title: string;
 
+  @ApiProperty({
+    description: 'The content',
+  })
   @Column('text')
   content: string;
 
   @Column()
+  @ApiProperty({
+    description: 'Description of the journal',
+  })
   category: string;
 
-  @Column({ type: 'date' })
+  @CreateDateColumn()
   date: Date;
 }
